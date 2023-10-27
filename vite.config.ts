b780dda3e65d4071e45e485f589b992ccb569497
Jan Stümmel <7893682+janstuemmel@users.noreply.github.com>
2023-10-27 19:24:52 +0200

    add node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [svelte()],
-  base: 'jsrepl',
+  plugins: [
+    svelte(),
+    nodePolyfills()
+  ],
+  base: '/jsrepl',
   build: {
-    outDir: 'jsrepl'
-  }
+    outDir: 'jsrepl',
+  },
 })
